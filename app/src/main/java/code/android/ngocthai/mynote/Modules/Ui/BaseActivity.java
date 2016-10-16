c59de55f8c6700231fe43a687359dfbf2247fc2a
ngocthai69 <ngocthaihn2@gmail.com>
2016-10-16 10:14:16 +0700

    add function hide keyboard

--- app/src/main/java/code/android/ngocthai/mynote/Modules/Ui/BaseActivity.java
+++ app/src/main/java/code/android/ngocthai/mynote/Modules/Ui/BaseActivity.java
@@ -1,11 +1,13 @@
 package code.android.ngocthai.mynote.Modules.Ui;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 /**
  * Created by NguyenBao on 06/10/2016.
@@ -31,14 +33,32 @@ public abstract class BaseActivity extends AppCompatActivity {
         initData(savedInstanceState);
     }
 
-    public static void hideKeyboard(Activity activity) {
-        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        //Find the currently focused view, so we can grab the correct window token from it.
-        View view = activity.getCurrentFocus();
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = new View(activity);
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public static void hideKeyroard(View view, final Activity activity) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(activity);
+                    return false;
+                }
+            });
         }
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                hideKeyroard(innerView, activity);
+            }
+        }
     }
 }
