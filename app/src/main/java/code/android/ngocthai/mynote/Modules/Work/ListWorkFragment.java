7489bdcbb2b11666e6e7c93dd0105e98a45ff843
ngocthai69 <ngocthaihn2@gmail.com>
2016-10-16 10:57:57 +0700

    no change

--- app/src/main/java/code/android/ngocthai/mynote/Modules/Work/ListWorkFragment.java
+++ app/src/main/java/code/android/ngocthai/mynote/Modules/Work/ListWorkFragment.java
@@ -1,30 +1,85 @@
 package code.android.ngocthai.mynote.Modules.Work;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import code.android.ngocthai.mynote.Common.Adapter.ListNoteAdapter;
+import code.android.ngocthai.mynote.Common.Adapter.ListWorkAdapter;
+import code.android.ngocthai.mynote.Common.Object.Note;
+import code.android.ngocthai.mynote.Common.Object.Work;
+import code.android.ngocthai.mynote.Common.Utils.Constraint;
+import code.android.ngocthai.mynote.Common.Utils.RecyclerTouchListener;
+import code.android.ngocthai.mynote.Data.Client.DBController;
+import code.android.ngocthai.mynote.Modules.Note.EditNoteActivity;
+import code.android.ngocthai.mynote.Modules.Ui.BaseFragment;
 import code.android.ngocthai.mynote.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListWorkFragment extends Fragment {
+public class ListWorkFragment extends BaseFragment {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Work> listWork;
+    private ListWorkAdapter adapter;
+    private LinearLayoutManager llm;
 
 
-    public ListWorkFragment() {
-        // Required empty public constructor
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.fragment_list_work;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_work, container, false);
+    protected void initVariables(Bundle saveInstanceState, View rootView) {
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.rcvListWork);
     }
+
+    @Override
+    protected void initData(Bundle saveInstanceState) {
+        recyclerView.setHasFixedSize(true);
+        listWork = queryData();
+        adapter = new ListWorkAdapter(listWork);
+        llm = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Work work = listWork.get(position);
+                Toast.makeText(getActivity(), "" + work.getID(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), EditWorkActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+    }
+
+    public ArrayList<Work> queryData() {
+        ArrayList<Work> ls = new ArrayList<>();
+        DBController db = new DBController(getActivity());
+        ls = db.getAllWork();
+        return ls;
+    }
+
 
 }
