14d72beb71e2aec00879b11d66b45f73c0daf437
ngocthai69 <ngocthaihn2@gmail.com>
2016-10-16 18:03:28 +0700

    add function get current time

--- app/src/main/java/code/android/ngocthai/mynote/Common/Utils/Utils.java
+++ app/src/main/java/code/android/ngocthai/mynote/Common/Utils/Utils.java
@@ -1,8 +1,24 @@
 package code.android.ngocthai.mynote.Common.Utils;
 
+import java.util.Calendar;
+
 /**
  * Created by Thaihn on 11/10/2016.
  */
 public class Utils {
 
+    /**
+     * Get current date to show list to do
+     *
+     * @return
+     */
+    public static String getCurrentDate() {
+        String result = "";
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH) + 1;
+        int date = cal.get(Calendar.DAY_OF_MONTH);
+        result = date + "-" + month + "-" + year;
+        return result;
+    }
 }
